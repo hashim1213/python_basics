8375e4d391188693fbfafa0913b4403e9e312cb7
hashim1213 <hashim200222@gmail.com>
2024-11-26 20:11:19 -0600

    variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+name = "Hashim" #String (text)
+age = 22 #Iterger, Whole number
+height = 1.75 #Float u
+is_student = False #boolean (one or the other )
+
+print("Hey " + name) #combine them with text
+print(age, height) #print the variables 
+
+print (f"{name} is {age} years old") # F-strigs 
+
+x = 2 
+y = 4 
+
+print(x + y)
+#...
+print(x % y)
